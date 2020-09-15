a0772f811aa595c957bb3d1b7528c12164428987
jadeson komolvipat <jadesonk@gmail.com>
2020-09-15 11:20:16 +0700

    fix typo in createMessage migration

--- db/migrate/20200915040556_create_messages.rb
+++ db/migrate/20200915040556_create_messages.rb
@@ -2,7 +2,7 @@ class CreateMessages < ActiveRecord::Migration[6.0]
   def change
     create_table :messages do |t|
       t.text :content
-      t.references :audtion, null: false, foreign_key: true
+      t.references :audition, null: false, foreign_key: true
       t.references :user, null: false, foreign_key: true
 
       t.timestamps
