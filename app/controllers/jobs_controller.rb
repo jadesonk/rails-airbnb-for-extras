521c422d2887d65588fc18e074edf37cd845a6cf
jadeson komolvipat <jadesonk@gmail.com>
2020-09-16 13:20:14 +0700

    create routes for new job listing

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -6,4 +6,25 @@ class JobsController < ApplicationController
   def show
     @job = Job.find(params[:id])
   end
+
+  def new
+    @job = Job.new
+  end
+
+  def create
+    @job = Job.new(job_params)
+    @job.user = current_user
+
+    if @job.save
+      redirect_to @job
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def job_params
+    params.require(:job).permit(:title, :description, :location, :application_deadline_date, :shoot_date)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "jobs", to: "jobs#index"
+  get "jobs/new", to: "jobs#new"
+  post "jobs", to: "jobs#create"
   get "jobs/:id", to: "jobs#show", as: :job
+
   get "performers", to: "users#index"
   get "performers/:id", to:"users#show", as: :performer
   get "performers/:id/edit", to:"users#edit", as: :edit_performer
